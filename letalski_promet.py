48f009744c106f1c5add25b1b1205cc358517ed0
gapro8 <kmeticgasper8@gmail.com>
2022-05-26 12:05:31 +0200

    popravil index.html

--- letalski_promet.py
+++ letalski_promet.py
@@ -2,6 +2,7 @@ import psycopg2, psycopg2.extensions, psycopg2.extras
 from bottle import *
 import auth
 import os
+import hashlib
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
@@ -17,7 +18,17 @@ debug(True)
 def index():
     cur.execute("SELECT (vzletno_letalisce, pristajalno_letalisce, cas_odhoda, cas_prihoda) FROM let;")
     leti = cur.fetchall()
-    return template('index.html', leti=leti)
+    # grdo sej vem :()
+    print(leti[0][0].split(','))
+    nleti = []
+    for let in leti:
+        let = let[0].split(',')
+        vzletno = let[0][2:-1]
+        pristajalno = let[1][1:-1]
+        cas_odhoda = let[2]
+        cas_prihoda = let[3][:-1]
+        nleti.append([vzletno, pristajalno, cas_odhoda, cas_prihoda])
+    return template('index.html', leti=nleti)
 
 @post('/leti/') # poizvedba za let
 def let():
@@ -44,6 +55,106 @@ def registracija():
 # def kupi_karto(id_leta, st_kart=1):
 #     cur.execute("insert into karta (st_narocila, razred, ime_potnika, cena, stevilka_sedeza, stevilka_leta) values (9,150, 'Boeing 767', 0);")
 #     return f"uspešno ste kupili karto za let {id_leta}"
+
+# TODO
+############################################
+### Registracija, prijava
+############################################
+def nastaviSporocilo(sporocilo = None):
+    # global napakaSporocilo
+    staro = request.get_cookie("sporocilo", secret=skrivnost)
+    if sporocilo is None:
+        response.delete_cookie('sporocilo')
+    else:
+        response.set_cookie('sporocilo', sporocilo, path="/", secret=skrivnost)
+    return staro 
+
+skrivnost = "banana"
+
+def hashGesla(s):
+    m = hashlib.sha256()
+    m.update(s.encode("utf-8"))
+    return m.hexdigest()
+
+@get('/registracija')
+def registracija_get():
+    napaka = nastaviSporocilo()
+    return template('registracija.html', napaka=napaka)
+
+@post('/registracija')
+def registracija_post():
+    emso = request.forms.emso
+    ime = request.forms.ime
+    priimek = request.forms.priimek
+    email = request.forms.email
+    telefon = request.forms.telefon
+    datum_rojstva = request.forms.datum_rojstva
+    uporabnisko_ime = request.forms.uporabnisko_ime
+    geslo = request.forms.geslo
+    geslo2 = request.forms.geslo2
+    if emso is None or uporabnisko_ime is None or geslo is None or geslo2 is None:
+        nastaviSporocilo('Registracija ni možna') 
+        redirect('/registracija')
+        return
+    # cur = baza.cursor()    
+    uporabnik = None
+    try: 
+        uporabnik = cur.execute("SELECT * FROM uporabnik WHERE emso = ?", (emso, )).fetchone()
+    except:
+        uporabnik = None
+    if uporabnik is None:
+        nastaviSporocilo('Registracija ni možna') 
+        redirect('/registracija')
+        return
+    if len(geslo) < 4:
+        nastaviSporocilo('Geslo mora imeti vsaj 4 znake.') 
+        redirect('/registracija')
+        return
+    if geslo != geslo2:
+        nastaviSporocilo('Gesli se ne ujemata.') 
+        redirect('/registracija')
+        return
+    zgostitev = hashGesla(geslo)
+    cur.execute("UPDATE uporabnik SET uporabnisko_ime = ?, geslo = ? WHERE emso = ?", (uporabnisko_ime, zgostitev, emso))
+    response.set_cookie('uporabnisko_ime', uporabnisko_ime, secret=skrivnost)
+    redirect('/')
+
+
+@get('/prijava')
+def prijava_get():
+    napaka = nastaviSporocilo()
+    return template('prijava.html', napaka=napaka)
+
+@post('/prijava')
+def prijava_post():
+    uporabnisko_ime = request.forms.uporabnisko_ime
+    geslo = request.forms.geslo
+    if uporabnisko_ime is None or geslo is None:
+        nastaviSporocilo('Uporabniško ima in geslo morata biti neprazna') 
+        redirect('/prijava')
+        return
+    # cur = baza.cursor()    
+    hashBaza = None
+    try: 
+        hashBaza = cur.execute("SELECT geslo FROM uporabnik WHERE uporabnisko_ime = ?", (uporabnisko_ime, )).fetchone()
+        hashBaza = hashBaza[0]
+    except:
+        hashBaza = None
+    if hashBaza is None:
+        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
+        redirect('/prijava')
+        return
+    if hashGesla(geslo) != hashBaza:
+        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
+        redirect('/prijava')
+        return
+    response.set_cookie('uporabnisko_ime', uporabnisko_ime, secret=skrivnost)
+    redirect('/')
+    
+@get('/odjava')
+def odjava_get():
+    response.delete_cookie('uporabnisko_ime')
+    redirect('/prijava')
 
 
 ####################################################
